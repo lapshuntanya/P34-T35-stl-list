9a0a02f58cbbfd47a6b126336f2944e88bc07870
lapshuntanya <lapshun.tatiana@gmail.com>
2024-11-21 16:51:49 +0200

    main

--- P34-T35-stl-list/P34-T35-stl-list.cpp
+++ P34-T35-stl-list/P34-T35-stl-list.cpp
@@ -1,7 +1,28 @@
-#include <iostream>
+#include "MyList.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    using namespace mylist;
+
+    //7 4 9 2
+
+    Node<int> first(7); //info =7, next =nullptr
+
+    Node<int> second(4); // info=4, next=nullptr
+    first.next = &second;
+
+    Node<int> third(9); //info=9, next=nullptr
+    second.next = &third;
+
+    Node<int> fourth(2); //info=2, next=nullptr
+    third.next = &fourth;
+
+    //Обхід списку
+    Node<int>* p = &first;
+    while (p != nullptr) {
+        cout << p->info << endl;
+        p->showNode();
+        p = p->next;
+    }
 }
 
